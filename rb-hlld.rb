9476c09bc1350b87a5bf18bc779f25c2c13d6a05
Matt Layher <mdlayher@gmail.com>
2013-11-12 18:33:34 -0500

    Add methods to create and drop HLL sets, add method to send and receive socket messages

--- rb-hlld.rb
+++ rb-hlld.rb
@@ -1,6 +1,9 @@
 require 'socket'
 
 class HlldClient
+	# Constants for string responses from hlld
+	HLLD_DONE = "Done"
+
 	# Initializer, set host and port
 	def initialize(host, port = 4553)
 		@host = host
@@ -34,5 +37,43 @@ class HlldClient
 		end
 
 		return false
+	end
+
+	# Create a HLL set on server
+	# TODO: named parameters to specify either precision OR epsilon.  For now, just precision.
+	def create(name, precision = nil, in_memory = nil)
+		# Begin building command to send to server
+		buffer = "create " + name + " "
+
+		# If specified, send precision
+		if precision.nil? and precision.is_a? Float
+			buffer += "precision= " + precision + " "
+		end
+
+		# If specified, choose if set should reside in memory
+		if in_memory.nil? and !!in_memory == in_memory
+			buffer += "in_memory=" + in_memory ? 1 : 0
+		end
+
+		# Send create set request to server, verify done
+		send(buffer) == HLLD_DONE
+	end
+
+	# Drop a HLL set on server
+	def drop(name)
+		send("drop " + name) == HLLD_DONE
+	end
+
+	private
+
+	# Send a message to server on socket
+	def send(input)
+		if !@connected || @socket.nil?
+			raise "Error: client is not connected to hlld server!"
+		end
+
+		# Write message, read reply
+		@socket.puts(input + "\n")
+		response = @socket.gets.chomp()
 	end
 end
